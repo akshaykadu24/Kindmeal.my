532ee63dac13986580801a2f872ccf21760784d3
vedpaswan01 <vedpaswan01@gmail.com>
2022-07-21 15:10:03 +0530

    changes folder structure

--- components/navbar.js
+++ components/navbar.js
@@ -1,29 +1,35 @@
 let navbar = () => {
-  return ` 
-    <div id="upr">
-            <div id="logo"><img src="./navbar_images/logo.PNG" alt="" /></div>
-            <div id="social">
-           <img src="./navbar_images/social_1.jpg" alt="" />
-           <a href="https://www.facebook.com/KindMeal.my"><img src="./navbar_images/social_2.jpg" alt="" /></a>
-           <a href="https://twitter.com/KindMeal"><img src="./navbar_images/social_3.jpg" alt="" /></a>
-            </div>
-            <div id="loginAndsignup">
-              <p>login</p>
-              <button>Facebook</button>
-              <button>Email</button>
-              <p>Signup</p>
-            </div>
-          </div>
-          <div id="lower">
-            <div>Home</div>
-            <div>Meal Deals</div>
-            <div>KindMoments</div>
-            <div>Hot Picks</div>
-            <div>Recipes</div>
-            <div>Directory</div>
-            <div>Articles</div>
-            <div>Help</div>
-          </div>`;
+  return `<div id="upr">
+  <div id="logo">
+    <img src="./images/navbar_images/logo.PNG" alt="" />
+  </div>
+  <div id="social">
+    <img src="./images/navbar_images/social_1.jpg" alt="" />
+    <a href="https://www.facebook.com/KindMeal.my"
+      ><img src="./images/navbar_images/social_2.jpg" alt=""
+    /></a>
+    <a href="https://twitter.com/KindMeal"
+      ><img src="./images/navbar_images/social_3.jpg" alt=""
+    /></a>
+  </div>
+  <div id="loginAndsignup">
+    <p>login</p>
+    <button>Facebook</button>
+    <button>Email</button>
+    <p>Signup</p>
+  </div>
+</div>
+<div id="lower">
+  <div>Home</div>
+  <div>Meal Deals</div>
+  <div>KindMoments</div>
+  <div>Hot Picks</div>
+  <div>Recipes</div>
+  <div>Directory</div>
+  <div>Articles</div>
+  <div>Help</div>
+</div> 
+   `;
 };
 
 export default navbar;
