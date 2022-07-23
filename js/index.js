5472aae1222f0a6b0ae654c3e074c8ac1aef371b
vedpaswan01 <vedpaswan01@gmail.com>
2022-07-23 07:44:39 +0530

    fixed bottom part

--- js/index.js
+++ js/index.js
@@ -2,3 +2,4 @@ import navbar from "../components/navbar.js";
 import footer from "../components/footer.js";
 
 document.getElementById("navbar").innerHTML = navbar();
+document.getElementById("footer").innerHTML = footer();
