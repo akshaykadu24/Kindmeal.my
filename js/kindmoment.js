b7dd57344b5416f331451e1f8f3301ed290f89f2
prashantshetti07 <prashantshetti007@gmail.com>
2022-07-23 13:33:20 +0530

    kind

---
+++ js/kindmoment.js
@@ -0,0 +1,5 @@
+import navbar from "../components/navbar.js";
+import footer from "../components/footer.js";
+
+document.getElementById("navbar").innerHTML = navbar();
+document.getElementById("footer").innerHTML = footer();
