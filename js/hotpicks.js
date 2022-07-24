f924e2df13eb026ecc9b7352bbfc4e5539d92ab2
prashantshetti07 <prashantshetti007@gmail.com>
2022-07-24 19:54:58 +0530

    hotpicks

--- js/hotpicks.js
+++ js/hotpicks.js
@@ -435,3 +435,25 @@ eighthdetail1a.map(function(el){
 
 })
 
+
+// ------------------login ---------------------------------------
+
+let y = localStorage.getItem("loginstatus");
+let logout = () => {
+    let x = false;
+    localStorage.setItem("loginstatus", x)
+    window.location.href = "artical.html";
+};
+if (y) {
+    let z = JSON.parse(localStorage.getItem("UserDetails"))
+    let x = document.getElementById("loginAndsignup");
+    x.innerHTML = null;
+    let name = document.createElement("p");
+    name.innerText = `${z[0].UserName}`;
+    //   name.style.color = "black";
+    let lgt = document.createElement("p");
+    lgt.innerText = "logout";
+    lgt.addEventListener("click", logout);
+    x.append(name, lgt);
+}
+
